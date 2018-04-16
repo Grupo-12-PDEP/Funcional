eadca77ee8131244b0c8c266977077f1d4bc96db
javiernano <37817964+javiernano@users.noreply.github.com>
2018-04-16 14:37:49 -0300

    Agrego Testing y unos detallitos
    Agregue el testing de esta version y agregue los tipos Plata y Nombre, para dar mas expresividad.

--- pruebaTp1.hs
+++ pruebaTp1.hs
@@ -7,16 +7,17 @@ import Test.Hspec
 
 --Eventos
 
-type Billetera = Float
---Definimos el tipo Billetera como punto flotante para representar el dinero de
---la cuenta.
+type Plata = Float
+type Billetera = Plata
+--Definimos el tipo Billetera como Plata, y a su vez el tipo Plata como punto
+--flotante para representar el dinero de la cuenta.
 
 
 type Evento = Billetera -> Billetera
 --Definimos el Evento como algo que acontece sobre una billetera y devuelve una
 --nueva billetera con su valor afectado.
 
-deposito :: Float -> Evento
+deposito :: Plata -> Evento
 deposito monto | monto > 0 = (+) monto
                | otherwise = error "el monto debe ser positivo"
 
@@ -24,7 +25,7 @@ upgrade :: Evento
 upgrade unaBilletera | unaBilletera * 0.2 <= 10 = unaBilletera * 1.2
                      | otherwise = unaBilletera + 10
 
-extraccion :: Float -> Evento
+extraccion :: Plata -> Evento
 extraccion monto unaBilletera | monto < 0 = error "el monto debe ser positivo"
                               | unaBilletera - monto < 0 = 0
                               | otherwise = unaBilletera - monto
@@ -38,20 +39,16 @@ quedaIgual unaBilletera = unaBilletera
 
 --Usuarios
 
+type Nombre = String
+
 data Usuario = Usuario {
-     nombre :: String,
+     nombre :: Nombre,
      billetera :: Billetera
 } deriving (Show)
 
-
-
 pepe = Usuario "Jose" 10
-
 lucho = Usuario "Luciano" 2
-
 pepe2 = Usuario "Jose" 20
-
-
 
 
 -- Transacciones
@@ -60,7 +57,7 @@ type Transaccion = Usuario -> Evento
 --La transaccion la definimos como una funcion que recibe como parametro el
 --usuario a quien se le aplica, y devuelve el evento que produce en su billetera
 
-crearTransaccion :: String -> Evento -> Transaccion
+crearTransaccion :: Nombre -> Evento -> Transaccion
 crearTransaccion unNombre unEvento aQuienLeAplica | unNombre == nombre aQuienLeAplica = unEvento
                                                   | otherwise = quedaIgual
 
@@ -93,7 +90,7 @@ transaccionCuatro = crearTransaccion "Luciano" ahorroErrante
 
 --Pago entre usuarios
 
-crearPago :: String -> String -> Float -> Transaccion
+crearPago :: Nombre -> Nombre -> Plata -> Transaccion
 crearPago quienPaga quienRecibe monto aQuienLeAplica | nombre aQuienLeAplica == quienPaga = extraccion monto
                                                      | nombre aQuienLeAplica == quienRecibe = deposito monto
                                                      | otherwise = quedaIgual
@@ -101,9 +98,34 @@ crearPago quienPaga quienRecibe monto aQuienLeAplica | nombre aQuienLeAplica == 
 --La funcion crearPago como parametros el monto y los involucrados en la
 --transaccion, y devuelve una transaccion aplicable a cualquier usuarios
 
-transaccionCinco = crearPago "Jose" "Luciano" 5
+transaccionCinco = crearPago "Jose" "Luciano" 7
 
 
 
 
---
+--Testing
+
+testing = hspec $ do
+  describe "Tests de eventos sobre una billetera de 10 monedas" $ do
+    it "1. Depositar 10 más. Debería quedar con 20 monedas." $ deposito 10 10 `shouldBe` 20
+    it "2. Extraer 3: Debería quedar con 7." $ extraccion 3 10 `shouldBe` 7
+    it "3. Extraer 15: Debería quedar con 0." $ extraccion 15 10 `shouldBe` 0
+    it "4. Un upgrade: Debería quedar con 12." $ upgrade 10 `shouldBe` 12
+    it "5. Cerrar la cuenta: 0." $ cierreCuenta 10 `shouldBe` 0
+    it "6. Queda igual: 10." $ quedaIgual 10 `shouldBe` 10
+    it "7. Depositar 1000, y luego tener un upgrade: 1020." $ (upgrade.(deposito 1000)) 10 `shouldBe` 1020
+  describe "Tests sobre los usuarios definidos" $ do
+    it "8. ¿Cuál es la billetera de Pepe? Debería ser 10 monedas." $ billetera pepe `shouldBe` 10
+    it "9. ¿Cuál es la billetera de Pepe, luego de un cierre de su cuenta? Debería ser 0." $ (cierreCuenta.billetera) pepe `shouldBe` 0
+    it "10. ¿Cómo quedaría la billetera de Pepe si le depositan 15 monedas, extrae 2, y tiene un Upgrade? Debería quedar en 27.6." $ (upgrade.(extraccion 2).(deposito 15).billetera) pepe `shouldBe` 27.6
+  describe "Tests sobre transacciones" $ do
+    it "11. Aplicar la transacción 1 a Pepe. Esto debería producir el evento quedaIgual, que si se aplicara a una billetera de 20 monedas, deberá dar una billetera con ese mismo monto." $ (transaccionUno pepe) 20 `shouldBe` 20
+    it "12. Hacer que la transacción 2 se aplique a Pepe. El resultado, deberá ser el evento de depositar 5 monedas. Aplicarlo a una billetera de 10 monedas, mostrando que queda con 15." $ (transaccionDos pepe) 10 `shouldBe` 15
+    it "13. Hacer que la transacción 2 se aplique al nuevo Pepe. Aplicar el evento resultante a una billetera de 50 monedas, y verificar que aumenta quedando con 55." $ (transaccionDos pepe2) 50 `shouldBe` 55
+  describe "Tests sobre nuevos eventos" $ do
+    it "14. Aplicar la transaccion 3 a Lucho. Aplicar el evento resultante a una billetera de 10 monedas. Debe quedar con 0." $ transaccionTres lucho 10 `shouldBe` 0
+    it "15. Aplicar la transaccion 4 a Lucho. Aplicar el evento resultante a una billetera de 10 monedas. Debe quedar con 34." $ transaccionCuatro lucho 10 `shouldBe` 34
+    it "16. Aplicar la transacción 5 a Pepe. Debería causar el evento de extracción de 7 unidades. Al aplicarlo a una billetera de 10 monedas, debería dar una nueva billetera de 3." $ transaccionCinco pepe 10 `shouldBe` 3
+    it "17. Aplicar la transacción 5 a Lucho. Debería causar el evento de depósito de 7 unidades. Al aplicarlo a una billetera de 10 monedas, quedando con 17." $ transaccionCinco lucho 10 `shouldBe` 17
+
+--Fin
