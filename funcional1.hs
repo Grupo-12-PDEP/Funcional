d49e1c98eacd71104ae700707ab3fdccc189e3c2
JavierNano <javiernano96@gmail.com>
2018-05-15 12:05:53 -0300

    peorBloque

--- funcional1.hs
+++ funcional1.hs
@@ -107,48 +107,38 @@ testingPrimeraEntrega = hspec $ do
     it "7. Depositar 1000, y luego tener un upgrade: 1020." $ (upgrade . deposito 1000) 10 `shouldBe` 1020
 
   describe "Tests sobre los usuarios definidos" $ do
-    it "8. ¿Cuál es la billetera de Pepe? Debería ser 10 monedas." $ billetera pepe `shouldBe` 10
-    it "9. ¿Cuál es la billetera de Pepe, luego de un cierre de su cuenta? Debería ser 0." $ (cierreCuenta . billetera) pepe `shouldBe` 0
-    it "10. ¿Cómo quedaría la billetera de Pepe si le depositan 15 monedas, extrae 2, y tiene un Upgrade? Debería quedar en 27.6." $ (upgrade . extraccion 2 . deposito 15 . billetera) pepe `shouldBe` 27.6
+    it "8. ¿Cuál es la billetera de Pepe? Debería ser 10 monedas."
+      $ billetera pepe `shouldBe` 10
+    it "9. ¿Cuál es la billetera de Pepe, luego de un cierre de su cuenta? Debería ser 0."
+      $ (cierreCuenta . billetera) pepe `shouldBe` 0
+    it "10. ¿Cómo quedaría la billetera de Pepe si le depositan 15 monedas, extrae 2, y tiene un Upgrade? Debería quedar en 27.6."
+      $ (upgrade . extraccion 2 . deposito 15 . billetera) pepe `shouldBe` 27.6
 
   describe "Tests sobre transacciones" $ do
-    it "11. Aplicar la transacción 1 a Pepe. Esto debería producir el evento quedaIgual, que si se aplicara a una billetera de 20 monedas, deberá dar una billetera con ese mismo monto." $ transaccionUno pepe 20 `shouldBe` 20
-    it "12. Hacer que la transacción 2 se aplique a Pepe. El resultado, deberá ser el evento de depositar 5 monedas. Aplicarlo a una billetera de 10 monedas, mostrando que queda con 15." $ transaccionDos pepe 10 `shouldBe` 15
-    it "13. Hacer que la transacción 2 se aplique al nuevo Pepe. Aplicar el evento resultante a una billetera de 50 monedas, y verificar que aumenta quedando con 55." $ transaccionDos pepe2 50 `shouldBe` 55
+    it "11. Aplicar la transacción 1 a Pepe. Esto debería producir el evento quedaIgual, que si se aplicara a una billetera de 20 monedas, deberá dar una billetera con ese mismo monto."
+      $ transaccionUno pepe 20 `shouldBe` 20
+    it "12. Hacer que la transacción 2 se aplique a Pepe. El resultado, deberá ser el evento de depositar 5 monedas. Aplicarlo a una billetera de 10 monedas, mostrando que queda con 15."
+      $ transaccionDos pepe 10 `shouldBe` 15
+    it "13. Hacer que la transacción 2 se aplique al nuevo Pepe. Aplicar el evento resultante a una billetera de 50 monedas, y verificar que aumenta quedando con 55."
+      $ transaccionDos pepe2 50 `shouldBe` 55
 
   describe "Tests sobre nuevos eventos" $ do
-    it "14. Aplicar la transaccion 3 a Lucho. Aplicar el evento resultante a una billetera de 10 monedas. Debe quedar con 0." $ transaccionTres lucho 10 `shouldBe` 0
-    it "15. Aplicar la transaccion 4 a Lucho. Aplicar el evento resultante a una billetera de 10 monedas. Debe quedar con 34." $ transaccionCuatro lucho 10 `shouldBe` 34
+    it "14. Aplicar la transaccion 3 a Lucho. Aplicar el evento resultante a una billetera de 10 monedas. Debe quedar con 0."
+      $ transaccionTres lucho 10 `shouldBe` 0
+    it "15. Aplicar la transaccion 4 a Lucho. Aplicar el evento resultante a una billetera de 10 monedas. Debe quedar con 34."
+      $ transaccionCuatro lucho 10 `shouldBe` 34
 
   describe "Tests sobre transacciones entre usuarios" $ do
-    it "16. Aplicar la transacción 5 a Pepe. Debería causar el evento de extracción de 7 unidades. Al aplicarlo a una billetera de 10 monedas, debería dar una nueva billetera de 3." $ transaccionCinco pepe 10 `shouldBe` 3
-    it "17. Aplicar la transacción 5 a Lucho. Debería causar el evento de depósito de 7 unidades. Al aplicarlo a una billetera de 10 monedas, quedando con 17." $ transaccionCinco lucho 10 `shouldBe` 17
+    it "16. Aplicar la transacción 5 a Pepe. Debería causar el evento de extracción de 7 unidades. Al aplicarlo a una billetera de 10 monedas, debería dar una nueva billetera de 3."
+      $ transaccionCinco pepe 10 `shouldBe` 3
+    it "17. Aplicar la transacción 5 a Lucho. Debería causar el evento de depósito de 7 unidades. Al aplicarlo a una billetera de 10 monedas, quedando con 17."
+      $ transaccionCinco lucho 10 `shouldBe` 17
+
+
+
 
 --                                                          ENTREGA II
 
---Testing
-
-testingSegundaEntrega = hspec $ do
-
-  describe "Tests de impacto" $ do
-    it "Impactar la transaccion 1 a Pepe" $ impactar transaccionUno pepe `shouldBe` pepe
-    it "Impactar la transaccion 5 a Lucho" $ impactar transaccionCinco lucho `shouldBe` lucho {billetera = 9}
-    it "Impactar la transaccion 5 y luego la 2 a Pepe" $ (.) (impactar transaccionDos) (impactar transaccionCinco) pepe `shouldBe` pepe {billetera = 8}
-
-  describe "Tests de bloque" $ do
-    it "Aplicar bloque 1 a Pepe" $ impactarBloque bloque1 pepe `shouldBe` pepe {billetera = 18}
-    it "Solo Pepe tendra un saldo mayor a 10 luego de aplicar el bloque 1" $ quienesQuedanConBilleteraMayorA 10 bloque1 [pepe, lucho] `shouldBe` [pepe]
-    it "Pepe queda con mas dinero luego de aplicar el bloque 1" $ elMasRicoLuegoDe (impactarBloque bloque1) [pepe, lucho] `shouldBe` pepe
-    it "Lucho queda con menos dinero luego de aplicar el bloque 1" $ elMasPobreLuegoDe (impactarBloque bloque1) [pepe, lucho] `shouldBe` lucho
-
-  describe "Tests de block chain" $ do
-    it "Para Pepe el peor bloque de la block chain fue el bloque 1" $ impactarBloque (elPeorDeLosBloques blockchain pepe) pepe `shouldBe` pepe {billetera = 18}
-    it "Pepe luego de la block chain posee 115 creditos en su billetera" $ aplicarBlockchain blockchain pepe `shouldBe` pepe {billetera = 115}
-    it "Pepe queda con 51 creditos si solo aplicamos los 3 primeros bloques" $ aplicarNBloques blockchain 3 pepe `shouldBe` pepe {billetera = 51}
-    it "El saldo total entre Lucho y Pepe luego de un block chain es 115" $ (sum . map billetera) (aplicarBlockchainAVariosUsuarios blockchain [lucho, pepe]) `shouldBe` 115
-
-  describe "Tests de block chain infinito" $ do
-    it "Pepe pasa los 10000 creditos luego de los 11 primeros bloques" $ cuantosBloquesSeNecesitanParaTener 10000 blockInf pepe `shouldBe` 11
 
 --Usuario luego de transacción
 
@@ -172,7 +162,7 @@ impactarBloque = flip (foldr impactar)
 quienesQuedanConBilleteraMayorA :: Plata -> Bloque -> [Usuario] -> [Usuario]
 quienesQuedanConBilleteraMayorA nCreditos unBloque = filter (\ unUsuario -> billetera (impactarBloque unBloque unUsuario) > nCreditos )
 
---Funciones para comparar las billeteras de un grupo de usuarios
+--Funciones para comparar las billeteras de un grupo de usuarios tras un bloque
 
 type Impacto = Usuario -> Usuario
 
@@ -184,10 +174,10 @@ comparoResultadosYDevuelvoOriginal unCriterio unImpacto unUsuario otroUsuario
   | otherwise = otroUsuario
 
 elMasRicoLuegoDe :: Impacto -> [Usuario] -> Usuario
-elMasRicoLuegoDe unImpacto usuarios = foldr (comparoResultadosYDevuelvoOriginal (>) unImpacto) (Usuario 0 "nadie tiene un mango") usuarios
+elMasRicoLuegoDe unImpacto usuarios = foldr1 (comparoResultadosYDevuelvoOriginal (>) unImpacto) usuarios
 
 elMasPobreLuegoDe :: Impacto -> [Usuario] -> Usuario
-elMasPobreLuegoDe unImpacto usuarios = foldr (comparoResultadosYDevuelvoOriginal (<) unImpacto) (Usuario 1000000 "todos son millonarios") usuarios
+elMasPobreLuegoDe unImpacto usuarios = foldr1 (comparoResultadosYDevuelvoOriginal (<) unImpacto) usuarios
 
 --BlockChain
 
@@ -204,14 +194,14 @@ blockchain = ( bloque2 : take 10 masBloque1 )
 
 --Funciones que permiten aplicar cadenas de bloques a usuarios
 
-elPeorDeLosBloques :: Blockchain -> Usuario -> Bloque
-elPeorDeLosBloques unaBlockchain unUsuario = peorBloque unaBlockchain unUsuario []
 
-peorBloque :: Blockchain -> Usuario -> Bloque -> Bloque
-peorBloque [] _ peorBloqueHastaAhora = peorBloqueHastaAhora
-peorBloque ( cabeza : cola ) unUsuario [] = peorBloque cola unUsuario cabeza
-peorBloque ( cabeza : cola ) unUsuario peorBloqueHastaAhora | billetera (impactarBloque cabeza unUsuario) < billetera (impactarBloque peorBloqueHastaAhora unUsuario) = peorBloque cola unUsuario cabeza
-                                                            | otherwise = peorBloque cola unUsuario peorBloqueHastaAhora
+comparoResultadosYDevuelvoBloqueOriginal :: CriterioSobreUsuarios -> Usuario -> Bloque -> Bloque -> Bloque
+comparoResultadosYDevuelvoBloqueOriginal unCriterio unUsuario unBloque otroBloque
+  | impactarBloque unBloque unUsuario `unCriterio` impactarBloque otroBloque unUsuario = unBloque
+  | otherwise = otroBloque
+
+peorBloque :: Blockchain -> Usuario -> Bloque
+peorBloque unBlockChain unUsuario = foldr1 (comparoResultadosYDevuelvoBloqueOriginal (<) unUsuario) unBlockChain
 
 aplicarBlockchain :: Blockchain -> Usuario -> Usuario
 aplicarBlockchain = flip (foldr impactarBloque)
@@ -237,5 +227,47 @@ cuantosBloquesSeNecesitanParaTener tantaPlata ( primerBloque : bloquesRestantes 
   | otherwise = 1 + cuantosBloquesSeNecesitanParaTener tantaPlata bloquesRestantes (impactarBloque primerBloque unUsuario)
 
 cuantosBloquesSeNecesitanParaTener _ [] _ = error "es imposible llegar a ese monton con este BlockChain"
+
+
+
+--Testing
+
+testingSegundaEntrega = hspec $ do
+
+  describe "Tests de impacto" $ do
+    it "Impactar la transaccion 1 a Pepe"
+      $ impactar transaccionUno pepe `shouldBe` pepe
+    it "Impactar la transaccion 5 a Lucho"
+      $ impactar transaccionCinco lucho `shouldBe` lucho {billetera = 9}
+    it "Impactar la transaccion 5 y luego la 2 a Pepe"
+      $ (.) (impactar transaccionDos) (impactar transaccionCinco) pepe `shouldBe` pepe {billetera = 8}
+
+  describe "Tests de bloque" $ do
+    it "Aplicar bloque 1 a Pepe"
+      $ impactarBloque bloque1 pepe `shouldBe` pepe {billetera = 18}
+    it "Solo Pepe tendra un saldo mayor a 10 luego de aplicar el bloque 1"
+      $ quienesQuedanConBilleteraMayorA 10 bloque1 [pepe, lucho] `shouldBe` [pepe]
+    it "Pepe queda con mas dinero luego de aplicar el bloque 1"
+      $ elMasRicoLuegoDe (impactarBloque bloque1) [pepe, lucho] `shouldBe` pepe
+    it "Lucho queda con menos dinero luego de aplicar el bloque 1"
+      $ elMasPobreLuegoDe (impactarBloque bloque1) [pepe, lucho] `shouldBe` lucho
+
+  describe "Tests de block chain" $ do
+    it "Para Pepe el peor bloque de la block chain fue el bloque 1"
+      $ impactarBloque (peorBloque blockchain pepe) pepe `shouldBe` pepe {billetera = 18}
+    it "Pepe luego de la block chain posee 115 creditos en su billetera"
+      $ aplicarBlockchain blockchain pepe `shouldBe` pepe {billetera = 115}
+    it "Pepe queda con 51 creditos si solo aplicamos los 3 primeros bloques"
+      $ aplicarNBloques blockchain 3 pepe `shouldBe` pepe {billetera = 51}
+    it "El saldo total entre Lucho y Pepe luego de un block chain es 115"
+      $ (sum . map billetera) (aplicarBlockchainAVariosUsuarios blockchain [lucho, pepe]) `shouldBe` 115
+
+  describe "Tests de block chain infinito" $ do
+    it "Pepe pasa los 10000 creditos luego de los 11 primeros bloques"
+      $ cuantosBloquesSeNecesitanParaTener 10000 blockInf pepe `shouldBe` 11
+
+
+
+
 
 --Fin
